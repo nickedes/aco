b6576de64b9641ed425799cfc2c92b6eece0a1f5
Zishan Ahmad <zishanahmad@outlook.com>
2015-01-26 22:18:17 +0530

    Completed phase 1

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,6 +1,6 @@
 # Iterate over Chaotic Logistic Map
 def chaotic(x, u, count):
-    """ Iterates over the chaotic logistic map """
+    """ Iterate over the chaotic logistic map """
 
     for i in range(count):
         x = u*x*(1-x)
@@ -8,6 +8,7 @@ def chaotic(x, u, count):
 
 # Iterate over Tent Map
 def tent(x, b, count):
+    """ Iterate over the tent map """
     for i in range(count):
         if 0 < x and x <= b:
             x = x/b
@@ -17,3 +18,30 @@ def tent(x, b, count):
             print "Something wrong at Tent Map"
     return x
 
+
+def pretty(sbox):
+    """ Return a pretty printed SBox.
+        This function is coded by Shadab Zafar
+        Refer: https://github.com/dufferzafar/substitute/blob/master/core/__init__.py
+    """
+
+    # List of Columns
+    p = '\n       '
+    for i in range(16):
+        p += '%02x' % i + '  '
+    p += '\n'
+
+    for i in range(70):
+        p += '-'
+    p += '\n'
+
+    # Row
+    for i in range(16):
+        p += '%02x' % i + '  |  '
+
+        # Entries
+        for j in range(16):
+            p += '%02x' % sbox[i*15 + j] + '  '
+        p += '\n'
+
+    return p.upper()
